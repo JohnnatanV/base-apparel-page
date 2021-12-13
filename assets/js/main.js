92cf8e61f8366a5d6cbdf33914387fc0c62524fd
Johnnatan <joh.villaneda@outlook.com>
2021-12-13 00:47:26 -0500

    Finishing details, script in proccess

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,8 @@
-console.log("BASE-APPAREL-COMMING-SOON  ");
+const btn = document.querySelector("btn");
+
+const form = document.forms["form"];
+
+console.log(form);
+const validate = () => {
+  form.email.value;
+};
