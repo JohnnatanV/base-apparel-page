5ef16a50e738057079d85e182a17509abd464ced
Johnnatan <joh.villaneda@outlook.com>
2021-12-13 23:18:16 -0500

    Finishing details, layout

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,19 @@
-const btn = document.querySelector("btn");
+const btn = document.querySelector("#btn");
 
-const form = document.forms["form"];
+const form = document.forms.form;
 
-console.log(form);
 const validate = () => {
-  form.email.value;
+  let format = new RegExp(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/);
+
+  document.getElementById("error").classList.add("error-state");
+  document.getElementById("error-text").classList.add("error-state");
+
+  if (!format.test(form.email.value)) {
+    form.email.addEventListener("invalid", () => {
+      document.getElementById("error").classList.remove("error-state");
+      document.getElementById("error-text").classList.remove("error-state");
+    });
+  }
 };
+
+btn.addEventListener("click", validate);
